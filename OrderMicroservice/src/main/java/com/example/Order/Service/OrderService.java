fb99b4680216dcf87c7638227ff8b29dcff7afa8
LAPTOP-7N6ULLAH\Admin <abdur.raffay@outlook.com>
2024-10-08 00:30:52 +0500

    Configured the producer to send in the message to the queue with the matching name.

--- OrderMicroservice/src/main/java/com/example/Order/Service/OrderService.java
+++ OrderMicroservice/src/main/java/com/example/Order/Service/OrderService.java
@@ -4,14 +4,25 @@ import com.example.Order.Model.Orders;
 import com.example.Order.Repo.IOrderRepo;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class OrderService {
+    @Autowired
+    private Queue queue;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
     private final IOrderRepo orderRepo;
     @Transactional
     public void processOrder(Orders order){
         orderRepo.save(order);
+    }
+
+    public void generateMessage(String message){
+        rabbitTemplate.convertAndSend(queue.getName(), message);
     }
 }
