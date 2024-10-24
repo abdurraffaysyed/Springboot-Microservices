6ea7f0e420d343c793392df81aebf41f3b8d7b35
abdurraffaysyed <79164396+abdurraffaysyed@users.noreply.github.com>
2024-10-25 00:55:00 +0500

    Added enable rabbit annotation

--- OrderMicroservice/src/main/java/com/example/Order/RabbitMqConfig.java
+++ OrderMicroservice/src/main/java/com/example/Order/RabbitMqConfig.java
@@ -1,10 +1,12 @@
 package com.example.Order;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableRabbit
 public class RabbitMqConfig {
     private static final String queueName = "sample_queue";
     @Bean
